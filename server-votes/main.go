10f261ee68722fd02ce74a28c75f626f98bc9ca6
michael <michael@m6800>
2022-06-20 20:05:56 -0700

    WIP see you next time

--- server-votes/main.go
+++ server-votes/main.go
@@ -1,24 +1,71 @@
 package main
 
 import (
+    "time"
+    "syscall"
+    "context"
     "os"
     "log"
+    "net/http"
+    "encoding/json"
+    "io"
+    "fmt"
+    "os/signal"
 )
 
-func main() {
+type RequestType struct {
+    Action string `json:"action"` // "vote" or "get-votes"
+    CompetitionName string `json:"competition_name"`
+    SingerName string `json:"singer_name"`
+}
 
-    if len(os.Args) < 2 {
-        panic("must pass two arg, 'competition name' and 'singer name'")
+func main() {
+    port := 9092
+    http.HandleFunc("/vote", vote)
+
+    server := &http.Server{Addr: fmt.Sprintf(":%s", port)}
+    if err := server.ListenAndServe(); err != http.ErrServerClosed {
+        log.Fatal(err)
     }
 
-    competitionName := os.Args[1]
-    singerName := os.Args[2]
+    stop := make(chan os.Signal, 1)
+    signal.Notify(stop, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-    // action := os.Args[1]
+    <-stop
+
+    const delay = 10
+    ctx, cancel := context.WithTimeout(context.Background(), delay * time.Second)
+    defer cancel()
+    if err := server.Shutdown(ctx); err != nil {
+        log.Println(err)
+        return
+    }
+    // GET /vote/?singer_name=batman&competition_name=contest-1
+
+    competitionName := "contest-1"
+    singerName := "michael"
 
     err := produce(competitionName, singerName)
     if err != nil {
         log.Println(err)
         return
+    }
+}
+
+func vote(rw http.ResponseWriter, req *http.Request) {
+    body, err := io.ReadAll(req.Body)
+    if err != nil {
+        log.Println(err)
+    }
+
+    var request RequestType
+    err = json.Unmarshal(body, &request)
+    if err != nil {
+        log.Println(err)
+    }
+
+    err = produce(request.CompetitionName, request.SingerName)
+    if err != nil {
+        log.Println(err)
     }
 }
